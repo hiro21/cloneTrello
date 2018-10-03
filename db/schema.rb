9ddb431dac740a17a5c503d7579b33e08fe7c953
hiro21 <hiswing8@gmail.com>
2018-10-03 13:53:39 +0900

    modify model lanes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181003012841) do
+ActiveRecord::Schema.define(version: 20181003021651) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,14 @@ ActiveRecord::Schema.define(version: 20181003012841) do
   create_table "boads", force: :cascade do |t|
     t.string "name"
     t.integer "laneId"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "lanes", force: :cascade do |t|
+    t.integer "laneid"
+    t.integer "boadid"
+    t.string "lanename"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
