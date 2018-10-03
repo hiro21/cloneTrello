7ca554fc2cdfb336720ce10d8fc88c8f8190b818
hiro21 <hiswing8@gmail.com>
2018-10-03 11:03:03 +0900

    change model boad modify type

--- db/migrate/20181003012841_create_boads.rb
+++ db/migrate/20181003012841_create_boads.rb
@@ -2,7 +2,7 @@ class CreateBoads < ActiveRecord::Migration[5.1]
   def change
     create_table :boads do |t|
       t.string :name
-      t.int :laneId
+      t.integer :laneId
 
       t.timestamps
     end
